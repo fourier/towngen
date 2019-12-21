450f469386959fe773ea53f2d06d83da21592942
Alexey Veretennikov <alexey.veretennikov@gmail.com>
2019-12-21 19:15:15 +0100

    Priorities queue implementation continue

--- prio-queue.lisp
+++ prio-queue.lisp
@@ -17,24 +17,25 @@
 
 
 (defmethod prio-queue-top ((q prio-queue))
-  (slot-value q 'top))
+  (tree-head (slot-value q 'top)))
 
 
 (defmethod prio-queue-merge ((q1 prio-queue) (q2 prio-queue))
-  (let ((new-top
-          (cond ((and (slot-boundp q1 'top)
-                      (slot-boundp q2 'top))
-                 (if (funcall (slot-value q1 'test-function)
-                              (slot-value q1 'top)
-                              (slot-value q2 'top))
-                     (slot-value q1 'top)
-                     (slot-value q2 'top)))
+  (let* ((test-f (slot-value q1 'test-function))
+         (new-top
+           (cond ((and (slot-boundp q1 'top)
+                       (slot-boundp q2 'top))
+                  (let ((t1 (tree-head (slot-value q1 'top)))
+                        (t2 (tree-head (slot-value q2 'top))))
+                    (if (funcall test-f t1 t2)
+                        (slot-value q1 'top)
+                        (slot-value q2 'top))))
                 ((slot-boundp q1 'top) (slot-value q1 'top))
                 ((slot-boundp q2 'top) (slot-value q2 'top))
                 (t (error "Attempt to merge to empty prio queues"))))
-        (q
-          (make-instance 'prio-queue
-                         :test (slot-value q1 'test-function))))
+         (q
+           (make-instance 'prio-queue
+                          :test test-f)))
     (setf (slot-value q 'top) new-top
           (slot-value q 'storage)
           (append (slot-value q1 'storage)
@@ -42,22 +43,39 @@
     q))
 
 
-(defmethod prio-queue-insert ((q prio-queue) el)
+(defmethod prio-queue-push ((q prio-queue) el)
   (let ((q1 (make-instance 'prio-queue
                            :test (slot-value q 'test-function))))
-    (setf (slot-value q1 'top) el
-          (slot-value q1 'storage) (make-tree el))
+    (setf (slot-value q1 'top) (make-tree el)
+          (slot-value q1 'storage) (list (make-tree el)))
     (prio-queue-merge q1 q)))
 
 
 (defmethod prio-queue-pop ((q prio-queue))
   (with-slots (top storage test-function) q
     ;; 1. save the top element and make its
-    ;; children roots of another trees
-    (let ((result top))
+    ;;    children roots of another trees
+    (let ((result top)
+                                        ;(lengths (make-hash-table))
+          )
+      (format t "was ~d~%" (length storage))
       (setf storage
-            (append (tree-children top) storage))
-      ;; 
+            (delete top storage)
+            ;;(append (tree-children top) storage))
+            )
+      (format t "now ~d~%" (length storage))
+      (setf top
+            (reduce (lambda (x y)
+                      (if (funcall test-function
+                                   (tree-head x)
+                                   (tree-head y))
+                          x y))
+                    storage))
+      (tree-head result))))
+      ;; ...and update top element
+      ;; 2. merge all the trees with the same number
+      ;;    of children
+      
     
               
 
