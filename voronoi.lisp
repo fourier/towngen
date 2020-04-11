da32f110388df51b5f110677e93f1e3681e13172
Alexey Veretennikov <alexey.veretennikov@gmail.com>
2020-04-11 20:41:28 +0200

    Added tests

--- voronoi.lisp
+++ voronoi.lisp
@@ -20,21 +20,23 @@
    (beachline :initform nil
               :reader voronoi-beachline
               :documentation "A beach line of the Fortune's method")
-   (sweepline :initform 0
-              :type float
-              :accessor voronoi-sweepline
-              :documentation "A y-coordinate of the sweepline")
    (queue :type prio-queue
           :reader voronoi-queue
           :documentation "Priority queue for the Fortune's method"))
   (:documentation "Fortune's algorithm for Voronoi diagrams"))
 
+(defmethod event-get-x ((node point))
+  (point-x node))
+
+(defmethod event-get-x ((evt circle-event))
+  (point-x (circle-event-node evt)))
 
 (defmethod event-get-y ((node point))
   (point-y node))
 
 (defmethod event-get-y ((evt circle-event))
   (point-y (circle-event-node evt)))
+
 
 
 (defmethod initialize-instance :after ((self voronoi)
@@ -45,7 +47,7 @@
     (setf queue (make-instance 'prio-queue
                                :test
                                (lambda (a b)
-                                 (> (event-get-y a)
+                                 (< (event-get-y a)
                                     (event-get-y b)))))
     (loop for node in nodes do (prio-queue-push queue node))))
 
@@ -54,17 +56,63 @@
 No generation performed yet"
   (make-instance 'voronoi :nodes nodes))
 
-(defmethod handle-voronoi-queue-event ((circle-event circle-event))
+(defmethod handle-voronoi-queue-event ((self voronoi) (circle-event circle-event))
   "Handle circle event of the Voronoi diagram"
   (format t "Circle event: ~a~%" circle-event))
 
-(defmethod handle-voronoi-queue-event ((point point))
+(defun make-btree (value)
+  (make-instance 'btree :value value
+                 :comparator
+                 (lambda (a b)
+                   (> (event-get-x a)
+                      (event-get-x b)))))
+
+(defmethod handle-voronoi-queue-event ((self voronoi) (point point))
   "Handle node event of the Voronoi diagram"
-    (format t "Node event: ~a~%" point))
+    (format t "Node event: ~a~%" point)
+    (with-slots (beachline) self
+    (if (null beachline)
+        (setf beachline (make-btree point))
+        nil)))
+                                            
 
 
 (defmethod voronoi-generate ((self voronoi))
+  ;; implementation follows the link
+  ;; https://web.archive.org/web/20110601031438/http://cgm.cs.mcgill.ca/~mcleish/644/Projects/DerekJohns/Sweep.htm#DataStructures
   (with-slots (nodes queue) self
     (loop for evt = (prio-queue-pop queue)
           while evt
-          do (handle-voronoi-queue-event evt))))
+          do (handle-voronoi-queue-event self evt)))
+  self)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; tests
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun test-simple-diagragm-10points ()
+  ;; from Wolfram Mathematica:
+  ;; pts = {{5.51989, 5.47211}, {5.31936, 5.18963}, {1.75213, 9.95644}, {3.22207, 7.62254}, {9.51712, 9.3285}, {4.20739, 2.09659}, {7.22886, 5.36499}, {8.76779, 1.28443}, {3.94233, 6.88071}, {9.98516, 9.91357}};
+  ;; VoronoiMesh[pts]
+  (let* ((pts '((5.51989 5.47211) 
+                (5.31936 5.18963)
+                (1.75213 9.95644)
+                (3.22207 7.62254)
+                (9.51712 9.3285)
+                (4.20739 2.09659)
+                (7.22886 5.36499)
+                (8.76779 1.28443)
+                (3.94233 6.88071)
+                (9.98516 9.91357)))
+         (nodes
+          (loop for (x y) in pts collect
+                (make-point :x x :y y)))
+         (v (make-instance 'voronoi :nodes nodes)))
+    (voronoi-generate v)))
+
+
+    
+                          
+             
+
+
