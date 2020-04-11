6ebdab425379562a94dd04c65e13811bd318bb98
Alexey Veretennikov <alexey.veretennikov@gmail.com>
2020-04-11 20:42:03 +0200

    Started implementation of the binary tree adapted for beach line in Fortune's algorithm

--- btree.lisp
+++ btree.lisp
@@ -14,6 +14,10 @@
                :initform #'<
                :reader comparator
                :documentation "Function used to compare 2 value")
+   (divider :initarg :divider
+            :reader btree-divider
+            :initform (lambda (x y) (/ (+ x y) 2.0))
+            :documentation "Calculates the new middle node between new inserted and old")
   (left :initform nil
         :initarg :left
         :type (or null btree)
@@ -24,20 +28,67 @@
          :type (or null btree)
          :accessor btree-right
          :documentation "Right leaf of the tree"))
-  (:documentation "Binary tree"))
+  (:documentation "Binary tree for Fortune's method.
+This binary tree has a property that there are no subtrees
+having only one leaf. All roots of the tree have either 0
+leafs, or 2."))
 
 (defmethod btree-< ((btree btree) value)
+  "Compare btree node with value"
   (funcall (slot-value btree 'comparator)
            (btree-value btree) value))
-                        
-(defmethod btree-insert ((btree btree) value)
-  "Insert the VALUE into the binary tree"
-  ;; decide where to put, left or right
+
+(defmethod btree-make-from ((btree btree) value)
+  "Create a new node with the same functions as old but
+with a new value"
+  (make-instance 'btree
+                 :value value
+                 :comparator (slot-value btree 'comparator)
+                 :divider (slot-value btree 'divider)))
+                 
+(defmethod btree-leaf-p ((btree btree))
+  "Determine if the btree is a leaf, i.e. has no other leafs"
   (with-slots (left right) btree
-    (if (btree-< btree value)
-        (if left (btree-insert left value)
-            (setf left (make-instance 'btree :value value)))
-        (if right (btree-insert right value)
-            (setf right (make-instance 'btree :value value))))))
+    (and (null left) (null right))))
+
+(defmethod btree-insert ((btree btree) new-value)
+  "Insert the VALUE into the binary tree.
+Returns the new root if necessary"
+  ;; decide where to put, left or right
+  (with-slots (left right value) btree
+    ;; case the first root
+    (if (btree-leaf-p btree)
+        ;; new root
+        (let ((new-leaf (btree-make-from btree new-value))
+              (new-root
+               (btree-make-from btree
+                                (funcall (btree-divider btree) 
+                                         value new-value)))
+              (crap (format t "Leaf ~a inserting ~a"
+                            value new-value)))
+          ;; where to place old root
+          (if (btree-< new-root value) ;; add to the right
+              (setf (btree-right new-root) btree
+                    (btree-left new-root) new-leaf)
+              ;; or add to left
+              (setf (btree-left new-root) btree
+                    (btree-right new-root) new-leaf))
+              new-root)
+        ;; the tree has other nodes, find the closest one
+        (progn 
+          (if (btree-< btree new-value)
+              (setf right (btree-insert right new-value))
+              (setf left (btree-insert left new-value)))
+          btree))))
 
 ;;(defmethod btree-remove ((btree btree) value)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; tests
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun test-btree ()
+  (let ((b (make-instance 'btree :value 10)))
+    (setf b (btree-insert b 5))
+    (setf b (btree-insert b 7))
+    b))
